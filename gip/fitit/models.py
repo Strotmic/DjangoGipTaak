a7fa8b6037ce9f2662a6d9bac3724e9ec392e34b
Tim Bleuzé <tim.bleuze@gmail.com>
2022-06-01 11:06:41 +0200

    setup in settings

--- gip/fitit/models.py
+++ gip/fitit/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+
+class UserProfileInfo(models.Model):
+    username = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
+
+    def __str__(self):
+        return self.user.username
 # Create your models here.
